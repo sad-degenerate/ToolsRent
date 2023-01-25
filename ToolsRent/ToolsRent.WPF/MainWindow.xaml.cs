e93fdbb43e432a207cd78ca2e21849b2469ce38f
sad_degenerate <mister.franz02@gmail.com>
2023-01-25 11:18:10 +0700

    added empty projects and first setup done

---
+++ ToolsRent/ToolsRent.WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ToolsRent.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
