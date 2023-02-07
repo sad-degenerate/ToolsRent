6c370bf431f2d78cf16975f1212b63ec8f291454
sad_degenerate <mister.franz02@gmail.com>
2023-02-07 15:34:49 +0700

    added base realization of data base context

---
+++ ToolsRent/ToolsRent.BL/DataBase/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToolsRent.BL.Model;
+
+namespace ToolsRent.BL.DataBase;
+
+public sealed class ApplicationContext : DbContext
+{
+    public DbSet<BasePassport> Passports { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<ClientStats> ClientStats { get; set; }
+    public DbSet<Human> Humans { get; set; }
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // TODO: перенести connectionString в app.xaml
+        // TODO: удалить лишнее из Nuget (EntityFrameWork)
+        const string connectionString = 
+            @"Server=localhost\SQLEXPRESS;Database=tools_rent;Trusted_Connection=True;TrustServerCertificate=True;";
+        optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.UseLazyLoadingProxies();
+        
+        base.OnConfiguring(optionsBuilder);
+    }
+}
