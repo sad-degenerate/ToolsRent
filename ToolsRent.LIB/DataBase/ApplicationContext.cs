4d1e1833c6117f2e6ad43a0690e89a212b788723
sad_degenerate <mister.franz02@gmail.com>
2023-05-04 15:33:26 +0700

    added EntityFramework to project, created application context

---
+++ ToolsRent.LIB/DataBase/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToolsRent.LIB.DataBase;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connectionString = @"Server=localhost\SQLEXPRESS;Database=tools_rent;Trusted_Connection=True;TrustServerCertificate=True;";
+        optionsBuilder.UseLazyLoadingProxies().UseSqlServer(connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+}
