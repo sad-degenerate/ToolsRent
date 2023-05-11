164c80fd8201c3ea9825c596334b46a3fe3967c5
sad_degenerate <mister.franz02@gmail.com>
2023-05-11 18:09:53 +0700

    add styles, created first UI

---
+++ ToolsRent.WPF/Pages/AddEditPages/AddEditRenterPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ToolsRent.WPF.Pages.AddEditPages;
+
+public partial class AddEditRenterPage : Page
+{
+    public AddEditRenterPage()
+    {
+        InitializeComponent();
+    }
+}
